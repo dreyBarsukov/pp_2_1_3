d6711b83d145900cf9ff9ce9e2c00d4fcb0909b4
Andrey Barsukov <andreybarsukov96@gmail.com>
2023-10-20 21:29:27 +0700

    s commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -18,10 +18,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
-    }
-    public Timer getTimer() {
-        return timer;
     }
 }
